9c53b0881f6d583c0b52baabb2d9141f11307591
Bhanu <bhanuprakash437@gmail.com>
2017-10-22 19:47:02 +0200

    rename 008 to 007

--- src/test/java/use_case_007.java
+++ src/test/java/use_case_007.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class use_case_008 extends Base_Class{
+public class use_case_007 extends Base_Class{
     public static WebDriver driver;
     /**
      * This is a helper method to get the browser instance before the test suite runs.
@@ -32,5 +32,29 @@ public class use_case_008 extends Base_Class{
        ListExpensesPage listExpensesPage = new ListExpensesPage(driver);
        String new_name = listExpensesPage.get_username();
        Assert.assertEquals(new_name, Global.NEW_USERNAME);
+    }
+
+    /**
+     * To testcase register a new user!
+     */
+    @Test
+    public void registerNewUser_fail()
+    {
+        driver = super.loadSite();
+        RegisterNewUserPage Registernewuserpage = new RegisterNewUserPage(driver);
+        driver.findElement(By.xpath("/html/body/div/div/a")).click();
+
+        // we are at the new page
+        driver = Registernewuserpage.Register_new_user(Global.USERNAME , Global.PASSWORD, Global.PASSWORD);
+
+
+        boolean status = false;
+
+        if(driver.getCurrentUrl().equalsIgnoreCase("http://thawing-shelf-73260.herokuapp.com/listexpenses"))
+        {
+            status = true;
+        }
+
+        Assert.assertEquals(status,false);
     }
 }
